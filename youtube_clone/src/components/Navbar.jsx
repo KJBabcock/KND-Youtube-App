5432970ebb6fd4883fb3948c7843269c189cbd50
SquatchTang <kyjaba@gmail.com>
2022-10-26 23:00:09 -0500

    Youtube complete

--- youtube_clone/src/components/Navbar.jsx
+++ youtube_clone/src/components/Navbar.jsx
@@ -8,6 +8,7 @@ const Navbar = () => (
   <Stack direction="row" alignItems="center" p={2} sx={{ position:  "sticky", background: '#000', top: 0, justifyContent: "space-between" }}>
     <Link to="/" style={{ display: "flex", alignItems: "center" }}>
       <img src={logo} alt="logo" height={45} />
+      <h1 style={{color: "#fff", margin: "1rem"}}>KDB Media</h1>
     </Link>
     <SearchBar />
   </Stack>
